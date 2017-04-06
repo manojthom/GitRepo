f33ac1cb9efa4c4c5ca7f56b1089fd6fe04e9c1d
manojthom <manojthom1414@gmail.com>
2017-04-06 21:04:44 +0530

    Add files via upload

---
+++ Path/TestPjt/TestPjt/src/src/TestPgm.java
@@ -0,0 +1,17 @@
+package src;
+
+public class TestPgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b;
+		a=10;
+		b = a;
+		System.out.println(""+a) ;
+		
+		
+
+	}
+
+}
